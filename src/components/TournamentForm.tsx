c0a3e1d9a06d7e42e83e546d7c6e280a625ff464
James Luong <jamesluong@hotmail.co.uk>
2022-10-12 18:41:43 +0100

    Stored tournament details on home, passing as props to form

--- src/components/TournamentForm.tsx
+++ src/components/TournamentForm.tsx
@@ -1,27 +1,52 @@
 import { useState } from "react";
+import useModalContext from "../hooks/useModalContext";
 
-function TournamentForm() {
+interface TournamentDetails {
+  player: string,
+  tournament: string,
+  elo: number,
+}
+interface TournamentFormProps{
+  tournamentDetails: TournamentDetails
+  setTournamentDetails: (input: TournamentDetails) => void
+}
 
-    const DEFAULT_ELO = 1500;
-    const [name, setName] = useState("");
-    const [elo, setElo] = useState(DEFAULT_ELO);
+function TournamentForm(props:TournamentFormProps) {
+
+    const [player, setPlayer] = useState(props.tournamentDetails.player);
+    const [tournament, setTournament] = useState(props.tournamentDetails.tournament);
+    const [elo, setElo] = useState(props.tournamentDetails.elo);
+    const modalContext = useModalContext();
+
+
     const formSubmit = (e: React.FormEvent) => {
       e.preventDefault();
-      console.log(name, elo);
+      const formDetails = {player, tournament, elo}
+      //save formDetails spread over old details before form opened (props.tournamentDetails)
+      const newDetails = {...props.tournamentDetails, ...formDetails };
+      props.setTournamentDetails(newDetails);
+      modalContext.closeModal();
+      
     }
 
     return (
       <form className="flex flex-col items-start space-y-1 [&>input]:border-2" onSubmit={formSubmit}>
-        <h2>Personal Details</h2>
+        <h2>Tournament Details</h2>
         <hr className="h-1 w-full"/>
-        <label htmlFor="name">Name: </label>
-        <input className="" name="name" defaultValue={name} onChange={(e) => {setName(e.target.value)}} required/>
-        <label htmlFor="elo">FIDE ELO: </label>
-        <input type={"number"} defaultValue={elo} name="elo" onChange={(e) => {setElo(parseInt(e.target.value))}} required/>
+
+        <label>Tournament Name: </label>
+        <input defaultValue={tournament} onChange={(e) => {setTournament(e.target.value)}} required/>
+
+        <label>Player: </label>
+        <input defaultValue={player} onChange={(e) => {setPlayer(e.target.value)}} required/>
+
+        <label>FIDE ELO: </label>
+        <input type={"number"} defaultValue={elo} onChange={(e) => {setElo(parseInt(e.target.value))}} required/>
+
         <h2>Rounds</h2>
         <hr className="h-1 w-full"/>
 
-          <input className="pill-button bg-red-400" type="submit" required/>
+        <input className="pill-button bg-green-400" type="submit" value={"Save"} required/>
 
       </form>
     );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import TournamentForm from "../components/TournamentForm";
 import useModalContext from "../hooks/useModalContext";
 
 function Home() {
 
   const modalContext = useModalContext();
+  const [tournamentDetails, setTournamentDetails] = useState(
+    {
+      player: "",
+      tournament: "",
+      elo: 1500
+    }
+  );
   return (
     <div>
         <h1>Home</h1>
         <div className="p-1 space-y-2">
           <button 
             className="bg-blue-400 pill-button"
-            onClick={() => {modalContext.openModal(<TournamentForm/>)}}>
+            onClick={() => {modalContext.openModal(<TournamentForm tournamentDetails={tournamentDetails} setTournamentDetails={setTournamentDetails}/>)}}>
               Edit Tournament Details 
           </button>
         </div>
